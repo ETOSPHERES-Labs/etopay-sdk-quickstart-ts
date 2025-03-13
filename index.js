bb4dddc4a66df07d83d33b550452e0d4ad086c21
kristi <k.fidani@etogruppe.com>
2025-03-13 12:54:12 +0000

    try to create a new user

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ import { CryptpaySdk } from "@cawaena/cawaena-sdk-wasm";
 import * as dotenv from 'dotenv';
 import { access } from "fs/promises";
 import { resolve } from "path";
-
 const WALLET_PIN = "12345";
 const WALLET_PASSWORD = "Strong+Wallet+Pa55word";
-
 async function main() {
     dotenv.config();
     // Initialize the SDK
     const sdk = await new CryptpaySdk();
-
     // Set the SDK configuration. Get it from the dashboard: https://dashboard.cawaena.com
     sdk.setConfig(JSON.stringify({
         auth_provider: "<authentication provider name>",
@@ -18,10 +15,8 @@ async function main() {
         storage_path: "/path/to/valid/folder",
         log_level: "info"
     }));
-
     // Create new user if user database not exists
     const pathUserDb = resolve("./tmp/sdk-user.db");
     access(pathUserDb).catch(() => sdk.createNewUser("testuser"));
 }
-
 main();
